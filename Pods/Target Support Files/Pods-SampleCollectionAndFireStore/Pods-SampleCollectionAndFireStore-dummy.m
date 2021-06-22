ac7220167fb2ee8a215badd821d8d80b4d1d1c09
SonoD206 <20cm0120@jec.ac.jp>
2021-06-22 19:48:27 +0900

    Initial Commit

---
+++ Pods/Target Support Files/Pods-SampleCollectionAndFireStore/Pods-SampleCollectionAndFireStore-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleCollectionAndFireStore : NSObject
+@end
+@implementation PodsDummy_Pods_SampleCollectionAndFireStore
+@end
---
+++ Pods/Target Support Files/Pods-SampleCollectionAndFireStoreTests/Pods-SampleCollectionAndFireStoreTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SampleCollectionAndFireStoreTests : NSObject
+@end
+@implementation PodsDummy_Pods_SampleCollectionAndFireStoreTests
+@end
