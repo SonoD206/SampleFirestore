983a8bc8f99af57c1a9e6910f75081cc8372fa0c
SonoD206 <20cm0120@jec.ac.jp>
2021-06-23 08:06:10 +0900

    .gitignore add

--- Pods/Target Support Files/Pods-SampleCollectionAndFireStore-SampleCollectionAndFireStoreUITests/Pods-SampleCollectionAndFireStore-SampleCollectionAndFireStoreUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleCollectionAndFireStore_SampleCollectionAndFireStoreUITests : NSObject
-@end
-@implementation PodsDummy_Pods_SampleCollectionAndFireStore_SampleCollectionAndFireStoreUITests
-@end
--- Pods/Target Support Files/Pods-SampleCollectionAndFireStoreTests/Pods-SampleCollectionAndFireStoreTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SampleCollectionAndFireStoreTests : NSObject
-@end
-@implementation PodsDummy_Pods_SampleCollectionAndFireStoreTests
-@end
